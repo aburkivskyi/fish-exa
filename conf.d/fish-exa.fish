6a78e2427b6aa7f0a2204655d0d311681f8bf1c1
aburkivskyi <aburkivskyi@outlook.com>
2021-11-22 21:43:12 +0100

    mod: conf.d/fish-exa.fish

--- conf.d/fish-exa.fish
+++ conf.d/fish-exa.fish
@@ -1,18 +1,33 @@
-alias l 'exa'
-alias ll 'exa_git'
-alias la 'exa $EXA_STANDARD_OPTIONS $EXA_LA_OPTIONS'
-alias ld 'exa $EXA_STANDARD_OPTIONS $EXA_LD_OPTIONS'
-alias lg 'exa $EXA_STANDARD_OPTIONS $EXA_LG_OPTIONS'
-alias le 'exa $EXA_STANDARD_OPTIONS $EXA_LE_OPTIONS'
-alias lt 'exa $EXA_STANDARD_OPTIONS $EXA_LT_OPTIONS'
+function l -w exa
+  command exa $argv
+end
+function ll -w exa_git
+  exa_git $argv
+end
+function la -w exa
+  command exa $EXA_STANDARD_OPTIONS $EXA_LA_OPTIONS $argv
+end
+function ld -w exa
+  command exa $EXA_STANDARD_OPTIONS $EXA_LD_OPTIONS $argv
+end
+function lg -w exa
+  command exa $EXA_STANDARD_OPTIONS $EXA_LG_OPTIONS $argv
+end
+function le -w exa
+  command exa $EXA_STANDARD_OPTIONS $EXA_LE_OPTIONS $argv
+end
+function lt -w exa
+  command exa $EXA_STANDARD_OPTIONS $EXA_LT_OPTIONS $argv
+end
 
 function __fish_exa_install --on-event fish-exa_install
-    set -Ux EXA_STANDARD_OPTIONS "--long" "--all" "--group" "--header"
-    set -Ux EXA_LA_OPTIONS "--binary" "--links" "--inode" "--blocks"
-    set -Ux EXA_LD_OPTIONS "--list-dirs"
-    set -Ux EXA_LG_OPTIONS "--git"
+    set -Ux EXA_STANDARD_OPTIONS "--all" "--color=auto" "--group-directories-first"
+    set -Ux EXA_LA_OPTIONS "--binary" "--links" "--long" "--inode" "--blocks"
+    set -Ux EXA_LD_OPTIONS "--only-dirs"
+    set -Ux EXA_LG_OPTIONS "--git" "--long" "--modified" "--time-style=long-iso" "--no-user" "--octal-permissions" "--no-permissions" "--sort modified"
     set -Ux EXA_LE_OPTIONS "--extended"
     set -Ux EXA_LT_OPTIONS "--tree" "--level"
+    set -Ux EXA_LL_OPTIONS "--long" "--numeric" "--time-style=iso" "--octal-permissions" "--no-permissions" "--no-filesize" "--modified"
 end
 
 function __fish_exa_update --on-event fish-exa_update
